2de68bf412b1d62e487b2dc5c7094738d2f76f7e
andrianisilpi92 <andrianisilpi92@gmail.com>
2016-11-02 08:30:13 +0700

    Add files via upload

---
+++ PC.js
@@ -0,0 +1,9 @@
+<center>
+<h2 id="daplong" style="color: daplong;">
+Please Wait...</h2>
+<script src="Kode_JavaScript" type="text/javascript"></script> <script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("http://3dprinterolnine.blogspot.co.id/2016/11/pcb-manufacturing-service.html?url=", "");
+document.write('<button id="download" class="Visit_Link" onclick="changeLink();" style="display: none;text-align:justify;margin-bottom:2px;">Visit Link</button>')
+</script></center>
